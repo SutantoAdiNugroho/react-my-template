36a8ad3b48cb765b8fa4a67d0300234bd200bbad
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2021-05-15 16:19:04 +0700

    configure webpack for prod and development

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "@babel/polyfill";
+
 const HelloWorld = () => {
   return <h1>Hello World</h1>;
 };
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ webpack.config.dev.js
@@ -0,0 +1,10 @@
+const merge = require("webpack-merge").merge;
+const baseConfig = require("./webpack.config.base");
+
+module.exports = merge(baseConfig, {
+  mode: "development",
+  devServer: {
+    port: 4050,
+  },
+  devtool: "source-map",
+});
